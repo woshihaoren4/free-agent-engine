53d20d6addb7833eb8f0f533749edb3323d50902
wangdong.teshin <wangdong.teshin@bytedance.com>
2026-04-19 14:06:38 +0800

    Update fae-engine

--- crates/fae-engine/src/engine.rs
+++ crates/fae-engine/src/engine.rs
@@ -1,25 +1,32 @@
 use std::sync::Arc;
-use fae_agent::{Agent, AgentRef, Env, EnvWatch, EnvironmentWatch, Session, Task, TaskResult};
+use fae_agent::{Agent, AgentRef, Env, Session, Task, TaskResult};
+use crate::task_executor::{TaskRuntime, TaskRuntimeRef};
+
+pub struct RecallAgentRef{
+    agent: AgentRef,
+    score : f32,
+}
 
 #[async_trait::async_trait]
 pub trait AgentLoader:Sync{
-    async fn load_agent(&self, agent_name: &str) -> anyhow::Result<AgentRef>;
+    async fn load(&self, agent_name: &str) -> anyhow::Result<AgentRef>;
+    async fn recall(&self, task_desc: &str,limit: usize) -> anyhow::Result<Vec<RecallAgentRef>>;
 }
 
-#[async_trait::async_trait]
-pub trait TaskExecutor:Sync{
-    async fn execute(&self, task: Task) -> anyhow::Result<TaskResult>;
-}
 
 #[derive(Clone)]
 pub struct EngineEnvLayer{
     loader: Arc<dyn AgentLoader + Send + 'static>,
     env : Env,
-    env_watch: EnvWatch,
 }
 
-#[derive(Clone)]
 pub struct AgentsEngine {
     layers: Vec<EngineEnvLayer>,
-    executor: Arc<dyn TaskExecutor + Send + 'static>,
+    executor: TaskRuntimeRef,
+}
+
+impl AgentsEngine {
+    pub fn new<RT:Into<TaskRuntimeRef>>(executor: RT) -> Self {
+        Self { layers: Vec::new(), executor:executor.into() }
+    }
 }
